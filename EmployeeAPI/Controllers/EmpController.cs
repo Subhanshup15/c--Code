89ef033b46c1333fb0bc89809c45ce9a077fadf1
Subhanshu Pardeshi <subhanshup.15@gmail.com>
2025-10-29 21:13:37 +0530

    -crud opration

--- EmployeeAPI/Controllers/EmpController.cs
+++ EmployeeAPI/Controllers/EmpController.cs
@@ -1,6 +1,40 @@
+// using Microsoft.AspNetCore.Mvc;
+// using EmployeeAPI.Models;
+// using System.Collections.Generic;
+
+// namespace EmployeeAPI.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class EmpController : ControllerBase
+//     {
+//         private static List<Employee> employees = new List<Employee>
+//         {
+//             new Employee { Id = 1, Name = "Sagar", Department = "IT", Salary = 50000 },
+//             new Employee { Id = 2, Name = "Ravi", Department = "HR", Salary = 45000 },
+//             new Employee { Id = 3, Name = "Sagar", Department = "IT", Salary = 50000 },
+
+//         };
+
+//         [HttpGet]
+//         public ActionResult<IEnumerable<Employee>> GetEmployees()
+//         {
+//             return Ok(employees);
+//         }
+
+//         [HttpPost]
+//         public ActionResult AddEmployee(Employee emp)
+//         {
+//             emp.Id = employees.Count + 1;
+//             employees.Add(emp);
+//             return Ok(new { message = "Employee added successfully", data = emp });
+//         }
+//     }
+// }
 using Microsoft.AspNetCore.Mvc;
 using EmployeeAPI.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EmployeeAPI.Controllers
 {
@@ -11,9 +45,7 @@ namespace EmployeeAPI.Controllers
         private static List<Employee> employees = new List<Employee>
         {
             new Employee { Id = 1, Name = "Sagar", Department = "IT", Salary = 50000 },
-            new Employee { Id = 2, Name = "Ravi", Department = "HR", Salary = 45000 },
-            new Employee { Id = 3, Name = "Sagar", Department = "IT", Salary = 50000 },
-
+            new Employee { Id = 2, Name = "Ravi", Department = "HR", Salary = 45000 }
         };
 
         [HttpGet]
@@ -25,9 +57,34 @@ namespace EmployeeAPI.Controllers
         [HttpPost]
         public ActionResult AddEmployee(Employee emp)
         {
-            emp.Id = employees.Count + 1;
+            emp.Id = employees.Count > 0 ? employees.Max(e => e.Id) + 1 : 1;
             employees.Add(emp);
             return Ok(new { message = "Employee added successfully", data = emp });
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateEmployee(int id, Employee updatedEmp)
+        {
+            var emp = employees.FirstOrDefault(e => e.Id == id);
+            if (emp == null)
+                return NotFound(new { message = "Employee not found" });
+
+            emp.Name = updatedEmp.Name;
+            emp.Department = updatedEmp.Department;
+            emp.Salary = updatedEmp.Salary;
+
+            return Ok(new { message = "Employee updated successfully", data = emp });
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            var emp = employees.FirstOrDefault(e => e.Id == id);
+            if (emp == null)
+                return NotFound(new { message = "Employee not found" });
+
+            employees.Remove(emp);
+            return Ok(new { message = "Employee deleted successfully" });
         }
     }
 }
--- EmployeeAPI/obj/Debug/net9.0/EmployeeAPI.AssemblyInfo.cs
+++ EmployeeAPI/obj/Debug/net9.0/EmployeeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32643eb980a6bf148deb22abf34c191086618260")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
